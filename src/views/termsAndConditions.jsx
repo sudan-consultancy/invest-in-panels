178767eacefaff1fad174d0ae9ebd326e57d7257
Prabjyot Sudan <sudanprabjyot@gmail.com>
2022-07-09 11:21:00 +0530

    initial terms and conditions

---
+++ src/views/termsAndConditions.jsx
@@ -0,0 +1,333 @@
+import React from "react";
+import { Helmet } from "react-helmet";
+import HeaderLanding from "../components/vr-landing/Header";
+import FooterFive from "../components/footer/FooterFive";
+import ScrollspyNav from "react-scrollspy-nav";
+const TermsAndConditions = (props) => {
+  return (
+    <div className="main-page-wrapper p0 font-gordita">
+         <Helmet>
+        <title>
+          Terms & Conditions || Vefes AI
+        </title>
+      </Helmet>
+      <HeaderLanding />
+
+ <div className="terms_and_policy">
+        <div className="container">
+          <ScrollspyNav
+            scrollTargetIds={["opt1", "opt2", "opt3", "opt4", "opt5", "opt6"]}
+            activeNavClass="active"
+            offset={170}
+            scrollDuration="300"
+          >
+            <div className="row align-items-start">
+              <div className="col-lg-4 sidenav-sticky">
+                <ul className="nav nav-tabs">
+                  <li className="nav-item">
+                    <a className="nav-link active" href="#opt1">
+                      1. Introduction
+                    </a>
+                  </li>
+                  <li className="nav-item">
+                    <a className="nav-link" href="#opt2">
+                      2. Products
+                    </a>
+                  </li>
+                  <li className="nav-item">
+                    <a className="nav-link" href="#opt3">
+                      3. Website
+                    </a>
+                  </li>
+                  <li className="nav-item">
+                    <a className="nav-link" href="#opt4">
+                      4. Disclaimer of Warranties 
+                    </a>
+                  </li>
+                  <li className="nav-item">
+                    <a className="nav-link" href="#opt5">
+                      5. Limitation of Liability
+                    </a>
+                  </li>
+                  <li className="nav-item">
+                    <a className="nav-link" href="#opt6">
+                      6. Indemification
+                    </a>
+                  </li>
+                  <li className="nav-item">
+                    <a className="nav-link" href="#opt7">
+                      6. Privacy
+                    </a>
+                  </li>
+                  <li className="nav-item">
+                    <a className="nav-link" href="#opt8">
+                      7. Agreement to be bound
+                    </a>
+                  </li>
+                   <li className="nav-item">
+                    <a className="nav-link" href="#opt9">
+                      7. General
+                    </a>
+                  </li>
+                </ul>
+              </div>
+
+              <div className="col-lg-8">
+                {/* Tab panes */}
+                <div className="tab-content ">
+                  <div id="opt1">
+                    <h2 className="font-gilroy-bold">Terms of Service Agreement</h2>
+                    <div className="update-date">LAST UPDATED: 09/07/2022</div>
+                    <h3>1. Introduction</h3>
+                    <p>
+                     This Terms of Service Agreement (the &quot;Agreement&quot;) governs your use of this website, vefes.ai
+(the &quot;Website&quot;), Vefes (&quot;Business Name&quot;) offer of products for purchase on this Website, or your
+purchase of products available on this Website. This Agreement includes, and incorporates by
+this reference, the policies and guidelines referenced below. Vefes reserves the right to change
+or revise the terms and conditions of this Agreement at any time by posting any changes or a
+revised Agreement on this Website. Vefes will alert you that changes or revisions have been
+made by indicating on the top of this Agreement the date it was last revised. The changed or
+revised Agreement will be effective immediately after it is posted on this Website. Your use of the
+Website following the posting any such changes or of a revised Agreement will constitute your
+acceptance of any such changes or revisions. Vefes encourages you to review this Agreement
+whenever you visit the Website to make sure that you understand the terms and conditions
+governing use of the Website. This Agreement does not alter in any way the terms or conditions
+of any other written agreement you may have with Vefes for other products or services. If you do
+not agree to this Agreement (including any referenced policies or guidelines), please immediately
+terminate your use of the Website. If you would like to print this Agreement, please click the print
+button on your browser toolbar.
+                    </p>
+                   
+                  </div>
+                  <div id="opt2">
+                    <h2 className="font-gilroy-bold">Products</h2>
+                    <p>
+                        Terms of Offer. This Website offers for sale certain products (the &quot;Products&quot;). By placing an order
+for Products through this Website, you agree to the terms set forth in this Agreement.
+                    </p>
+                    <h3>Customer Solicitation:</h3>
+                    <p>
+                      Unless you notify our third-party call center reps or direct Vefes sales reps, while they are calling
+you, of your desire to opt out from further direct company communications and solicitations, you
+are agreeing to continue to receive further emails and call solicitations Vefes and its designated in
+house or third party call team(s).
+                    </p>
+                    <h3>Opt Out Procedure:</h3>
+                    <p>
+                      We provide 3 easy ways to opt out of from future solicitations.
+                      <ol>
+                        <li>You may use the opt out link found in any email solicitation that you may receive.</li>
+                        <li>You may also choose to opt out, via sending your email address to: support@vefes.ai.</li>
+                        <li>You may send a written remove request to B-208, Kailas Business Park, Hiranandani Link
+Road, Vikhroli West, Mumbai - 400079.</li>
+                       
+                      </ol>
+                      <p>Proprietary Rights. Vefes has rights to all trademarks and trade dress and specific layouts of this
+webpage, including calls to action, text placement, images and other information.</p>
+                    </p>
+                    <p>
+                      You can contact us by emailing{" "}
+                      <a href="#">hi@deski.com</a>.
+                    </p>
+                    <h3>Goods &amp; Service Tax</h3>
+                    <p>If you purchase any Products, you will be responsible for paying any applicable Good &amp; Service
+Tax.</p>
+                  </div>
+                  <div id="opt3">
+                    <h2 className="font-gilroy-bold">Website</h2>
+                   
+                    <h3>Content; Intellectual Property; Third Party Links</h3>
+                    <p>
+                     In addition to making Products available, this Website also offers information and marketing
+materials. This Website also offers information, both directly and through indirect links to third-
+party websites, about renewable energy. Vefes does not always create the information offered on
+this Website; instead, the information is often gathered from other sources. To the extent that
+Vefes does create the content on this Website, such content is protected by intellectual property
+laws of the India, foreign nations, and international bodies. Unauthorized use of the material may
+violate copyright, trademark, and/or other laws. You acknowledge that your use of the content on
+this Website is for personal, noncommercial use. Any links to third-party websites are provided
+solely as a convenience to you. Vefes does not endorse the contents on any such third-party
+websites. Vefes is not responsible for the content of or any damage that may result from your
+access to or reliance on these third-party websites. If you link to third-party websites, you do so at
+your own risk.
+                    </p>
+                    
+                    <h3>Use of Website</h3>
+                    <p>
+                     Vefes is not responsible for any damages resulting from use of this website by anyone. You will
+not use the Website for illegal purposes. You will
+                    </p>
+                    <ol>
+                        <li>abide by all applicable local, state, national, and international laws and regulations in your use
+of the Website (including laws regarding intellectual property),</li>
+<li>not interfere with or disrupt the use and enjoyment of the Website by other users,</li>
+<li>not resell material on the Website,</li>
+<li>not engage, directly or indirectly, in transmission of &quot;spam&quot;, chain letters, junk mail or any
+other type of unsolicited communication, and</li>
+<li>not defame, harass, abuse, or disrupt other users of the Website License.</li>
+                    </ol>
+                    <p>
+                        By using this Website, you are granted a limited, non-exclusive, non-transferable right to use the
+content and materials on the Website in connection with your normal, noncommercial, use of the
+Website. You may not copy, reproduce, transmit, distribute, or create derivative works of such
+content or information without express written authorization from Vefes or the applicable third
+party (if third party content is at issue).
+                    </p>
+                    <h3>Posting</h3>
+                    <p>
+                      By posting, storing, or transmitting any content on the Website, you hereby grant Vefes a
+perpetual, worldwide, non-exclusive, royalty-free, assignable, right and license to use, copy,
+display, perform, create derivative works from, distribute, have distributed, transmit and assign
+such content in any form, in all media now known or hereinafter created, anywhere in the world.
+Vefes does not have the ability to control the nature of the user-generated content offered
+through the Website. You are solely responsible for your interactions with other users of the
+Website and any content you post. Vefes is not liable for any damage or harm resulting from any
+posts by or interactions between users. Vefes reserves the right, but has no obligation, to monitor
+interactions between and among users of the Website and to remove any content Vefes deems
+objectionable, in Vefes&#39; sole discretion.
+                    </p>
+                  </div>
+                  <div id="opt4">
+                    <h2 className="font-gilroy-bold">Disclaimer of Warranties</h2>
+                    {/* <div className="update-date">LAST UPDATED: 6/26/2020</div> */}
+                
+                    <p>
+                     Your use of this website are at your sole risk. The website and products are offered on an &quot;as is&quot;
+and &quot;as available&quot; basis. Vefes expressly disclaims all warranties of any kind, whether express or
+implied, including, but not limited to, implied warranties of merchantability, fitness for a particular
+purpose and non-infringement with respect to the products or website content, or any reliance
+upon or use of the website content or products.
+Without limiting the generality of the foregoing, Vefes makes no warranty:
+That the information provided on this website is accurate, reliable, complete, or timely.
+That the links to third-party websites are to information that is accurate, reliable, complete, or
+timely.
+No advice or information, whether oral or written, obtained by you from this website will create
+any warranty not expressly stated herein. 
+Regarding any products purchased or obtained through the website.
+Some jurisdictions do not allow the exclusion of certain warranties, so some of the above
+exclusions may not apply to you.
+                    </p>
+                    
+                   
+                  </div>
+                  <div id="opt5">
+                    <h2 className="font-gilroy-bold">Third Party Links</h2>
+                    <div className="update-date">LAST UPDATED: 6/26/2020</div>
+                    <h3>1. Introduction</h3>
+                    <p>
+                      We have taken a number of steps to comply with the GDPR.
+                      For more details about our compliance and how we handle
+                      your data please check our latest legal documents below:
+                    </p>
+                    <ul>
+                      <li>
+                        <a href="#">Privacy policy</a>
+                      </li>
+                      <li>
+                        <a href="#">Terms and conditions</a>
+                      </li>
+                    </ul>
+                    <h3>2. About us</h3>
+                    <p>
+                      When you use Ticket Tailor for selling tickets and
+                      collecting data from your attendees you are the “data
+                      controller” of all your attendees’ data. We are a “data
+                      processor”, which means that we won’t do anything with
+                      your attendees’ data other than what you need us to in
+                      order to provide our service. We won’t be sending your
+                      attendees any marketing emails, sharing their data with
+                      anyone and if you want us to delete the data, just ask.
+                      It’s your data. 100
+                    </p>
+                    <h3>3. Accessing our Website</h3>
+                    <p>
+                      The servers and databases that power Ticket Tailor are
+                      located in the EU in Ireland. We inevitably use a small
+                      number of third parties, some of which are outside of the
+                      EU, to provide our ticketing service and we have contracts
+                      with each of them that oblige them to comply with the
+                      GDPR. You can find the full list of these third parties
+                      and what we use them for here.
+                    </p>
+                  </div>
+                  <div id="opt6">
+                    <h2 className="font-gilroy-bold">GDPR</h2>
+                    <div className="update-date">LAST UPDATED: 6/26/2020</div>
+                    <h3>1. Introduction</h3>
+                    <p>
+                      We have taken a number of steps to comply with the GDPR.
+                      For more details about our compliance and how we handle
+                      your data please check our latest legal documents below:
+                    </p>
+                    <ul>
+                      <li>
+                        <a href="#">Privacy policy</a>
+                      </li>
+                      <li>
+                        <a href="#">Terms and conditions</a>
+                      </li>
+                    </ul>
+                    <h3>2. About us</h3>
+                    <p>
+                      When you use Ticket Tailor for selling tickets and
+                      collecting data from your attendees you are the “data
+                      controller” of all your attendees’ data. We are a “data
+                      processor”, which means that we won’t do anything with
+                      your attendees’ data other than what you need us to in
+                      order to provide our service. We won’t be sending your
+                      attendees any marketing emails, sharing their data with
+                      anyone and if you want us to delete the data, just ask.
+                      It’s your data. 100
+                    </p>
+                    <h3>3. Accessing our Website</h3>
+                    <p>
+                      The servers and databases that power Ticket Tailor are
+                      located in the EU in Ireland. We inevitably use a small
+                      number of third parties, some of which are outside of the
+                      EU, to provide our ticketing service and we have contracts
+                      with each of them that oblige them to comply with the
+                      GDPR. You can find the full list of these third parties
+                      and what we use them for here.
+                    </p>
+                  </div>
+                </div>
+                {/*  /.tab-content */}
+              </div>
+            </div>
+          </ScrollspyNav>
+        </div>
+      </div>
+       {/* =====================================================
+            Footer Style Seven
+        ===================================================== */}
+     <footer className="theme-footer-five mt-130 md-mt-100">
+        <div className="inner-container">
+          <div className="container">
+            <FooterFive />
+          </div>
+        </div>
+        {/* /.inner-container */}
+        <p className="copyright">
+          {" "}
+          Copyright @{new Date().getFullYear()}{" "}
+          <a
+            href="#"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Vefes
+          </a>{" "}
+          AI.
+        </p>
+      </footer>
+     </div>
+
+    
+
+      
+
+  
+  );
+};
+
+export default TermsAndConditions;
