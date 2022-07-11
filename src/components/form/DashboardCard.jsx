617065f2f83efc5990bcded8b72e0ff3e874f9de
pcbudge <pcbudgeofficial@gmail.com>
2022-07-11 14:43:19 +0530

    changes in dashboard cart

--- src/components/form/DashboardCard.jsx
+++ src/components/form/DashboardCard.jsx
@@ -27,109 +27,170 @@ const DashboardCard = (props) => {
 
   return (
     <>
-    <HeaderLanding onDashboard />
-    <div className="container" style={{ paddingTop: "10em" }}>
-      <div className="container py-3 rounded" style={Container}>
-        <div className="row align-items-center">``
-          <img
-            src="..."
-            alt="panel image comes here"
-            className="col-12 col-sm-4 col-lg-3"
-            height="300px"
-          />
-          <div className="col-12 col-sm-6">
-            <h4>Panel</h4>
-            <span className="text-muted">Count: {count}</span>
-          </div>
-          <div className="d-flex align-items-center col-12 col-sm-4 col-lg-2">
-            <button
-              onClick={() => {
-                count > 0 && setCount(count - 1);
-              }}
-              className="btn btn-danger border border-2 border-danger"
-            >
-              &minus;
-            </button>
-            <input
-              className="form-control"
-              type="number"
-              value={count}
-              min="0"
-              required
-              onChange={(e) => setCount(e.target.value)}
+      <HeaderLanding onDashboard />
+      <div className="container" style={{ Container, paddingTop: "10em" }}>
+        <div className={`row`}>
+          <div className="card" style={{ width: "25rem", height: "30rem" }}>
+            <img
+              style={{ height: "50%" }}
+              className="card-img-top"
+              alt="Card image cap"
             />
-            <button
-              onClick={() => {
-                setCount(count + 1);
-              }}
-              className="btn btn-success border border-2 border-success"
-            >
-              +
-            </button>
+            <div className="card-body">
+              <h5 className="card-title">Card title</h5>
+              <p className="card-text">
+                Some quick example text to build on the card title and make up
+                the bulk of the card's content.
+              </p>
+              <div className="d-flex col-6 float-right card-text">
+                <button
+                  onClick={() => {
+                    count > 0 && setCount(count - 1);
+                  }}
+                  className="btn btn-danger border border-2 border-danger"
+                >
+                  &minus;
+                </button>
+                <input
+                  className="form-control"
+                  type="number"
+                  value={count}
+                  min="0"
+                  required
+                  onChange={(e) => setCount(e.target.value)}
+                />
+                <button
+                  onClick={() => {
+                    setCount(count + 1);
+                  }}
+                  className="btn btn-success border border-2 border-success"
+                >
+                  +
+                </button>
+              </div>
+            </div>
+            <div class="card-footer row">
+              {!kyc && (
+                <div className="d-flex col-8 align-items-center mt-1">
+                  <span className="text-danger mr-3">
+                    Please complete your KYC first.{" "}
+                    <a href="/kyc" className="text-info">
+                      Complete here
+                    </a>
+                  </span>
+                </div>
+              )}
+              <button
+                className="theme-btn-one float-right col-4"
+                disabled={!kyc}
+                onClick={popup}
+              >
+                Buy
+              </button>
+            </div>
           </div>
+
+          {/* <div
+            style={{ backgroundColor: "lightgray", borderRadius: "1rem" }}
+            className="col-4 align-items-center"
+          >
+            ``
+            <img
+              src="..."
+              alt="panel image comes here"
+              className=" card col-12"
+              height="300px"
+            />
+            <div className="col-12 p-2 text-center">
+              <h4>Panel</h4>
+              <div className="text-muted">Count: {count}</div>
+              <div className="text-muted">Count: {count}</div>
+              <div className="text-muted">Count: {count}</div>
+            </div>
+            <div className=" d-flex float-right col-6 p-3">
+              <button
+                onClick={() => {
+                  count > 0 && setCount(count - 1);
+                }}
+                className="btn btn-danger border border-2 border-danger"
+              >
+                &minus;
+              </button>
+              <input
+                className="form-control"
+                type="number"
+                value={count}
+                min="0"
+                required
+                onChange={(e) => setCount(e.target.value)}
+              />
+              <button
+                onClick={() => {
+                  setCount(count + 1);
+                }}
+                className="btn btn-success border border-2 border-success"
+              >
+                +
+              </button>
+            </div>
+          </div>
+          <div className={`col-12 text-center`}>
+            <button className="theme-btn-one " disabled={!kyc} onClick={popup}>
+              Buy
+            </button>
+          </div> */}
         </div>
+
+        <Modal
+          isOpen={popupflag}
+          onRequestClose={popup}
+          contentLabel="Bank details"
+          className="custom-modal modal-contact-popup-one"
+          overlayClassName="custom-overlay"
+          closeTimeoutMS={500}
+        >
+          <main className="main-body box_inner modal-content clearfix">
+            <button className="close" onClick={popup}>
+              <img src="images/icon/close.svg" alt="close" />
+            </button>
+            <table className="table table-responsive mt-5">
+              <tbody>
+                <tr>
+                  <th>Count:</th>
+                  <td>{count}</td>
+                </tr>
+                <tr>
+                  <th>Cost:</th>
+                  <td>{count * 27000}</td>
+                </tr>
+                <tr>
+                  <th colSpan={2}>Bank Details:</th>
+                </tr>
+                <tr>
+                  <th>Account name:</th>
+                  <td>Alex Bob</td>
+                </tr>
+                <tr>
+                  <th>Mobile number:</th>
+                  <td>987654321</td>
+                </tr>
+                <tr>
+                  <th>IFSC code:</th>
+                  <td>456543456789</td>
+                </tr>
+                <tr>
+                  <th>Type of account:</th>
+                  <td>Saving</td>
+                </tr>
+                <tr>
+                  <th>MICR code of bank:</th>
+                  <td>5433456644</td>
+                </tr>
+              </tbody>
+            </table>
+          </main>
+        </Modal>
       </div>
-      <div className="d-flex align-items-center float-right mt-3">
-        <span className="text-danger mr-3">
-          Please complete your KYC first.{" "}
-          <a href="/kyc" className="text-info">
-            Complete here
-          </a>
-        </span>
-        <button className="theme-btn-one" disabled={!kyc} onClick={popup}>
-          Buy
-        </button>
-      </div>
-      <Modal
-        isOpen={popupflag}
-        onRequestClose={popup}
-        contentLabel="Bank details"
-        className="custom-modal modal-contact-popup-one"
-        overlayClassName="custom-overlay"
-        closeTimeoutMS={500}
-      >
-        <main className="main-body box_inner modal-content clearfix">
-          <button className="close" onClick={popup}>
-            <img src="images/icon/close.svg" alt="close" />
-          </button>
-          <table className="table table-responsive mt-5">
-            <tbody>
-              <tr>
-                <th>Count:</th>
-                <td>{count}</td>
-              </tr>
-              <tr>
-                <th>Cost:</th>
-                <td>{count * 27000}</td>
-              </tr>
-              <tr>
-                <th colSpan={2}>Bank Details:</th>
-              </tr>
-              <tr>
-                <th>Account name:</th>
-                <td>Alex Bob</td>
-              </tr>
-              <tr>
-                <th>Mobile number:</th>
-                <td>987654321</td>
-              </tr>
-              <tr>
-                <th>IFSC code:</th>
-                <td>456543456789</td>
-              </tr>
-              <tr>
-                <th>Type of account:</th>
-                <td>Saving</td>
-              </tr>
-              <tr>
-                <th>MICR code of bank:</th>
-                <td>5433456644</td>
-              </tr>
-            </tbody>
-          </table>
-        </main>
-      </Modal>
-    </div>
     </>
   );
 };
